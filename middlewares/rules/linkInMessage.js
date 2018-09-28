af1f18cbebd214369cc911f32d82ef4fd37400c3
KaMeHb-UA <marlock@etlgr.com>
2018-09-28 16:58:55 +0300

    Implemented link recognition

--- middlewares/rules/linkInMessage.js
+++ middlewares/rules/linkInMessage.js
@@ -1,3 +1,16 @@
-module.exports = () => {
-    //
+const targetAssets = [
+    'url',              // link
+    'text_link',        // "hidden" link
+    'mention',          // @user(group)name
+];
+/**
+ * @param {TelegrafContext} ctx 
+ */
+module.exports = ctx => {
+    if(ctx.message.entities instanceof Array){
+        for(var i = 0; i < ctx.message.entities.length; i++){
+            if(targetAssets.indexOf(ctx.message.entities[i].type) != -1) return true;
+        }
+    }
+    return false
 }
