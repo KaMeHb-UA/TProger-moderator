0f8e41805e04933dc163ee9bc0a837cd9e57af60
KaMeHb-UA <marlock@etlgr.com>
2018-09-28 18:10:52 +0300

    Initial implementation of "safe" require

--- middlewares/rules/index.js
+++ middlewares/rules/index.js
@@ -1,10 +1,32 @@
 require('json5/lib/register');
+const safeRequire = (allowedRequireExtensions => {
+    const cache = {};
+    return (
+        /**
+         * @param {String} name 
+         */
+        name => {
+            if(cache[name]) return cache[name];
+            for(var i = 0; i < allowedRequireExtensions.length; i++){
+                try{
+                    cache[name] = require(`${name}.${allowedRequireExtensions[i]}`);
+                    return cache[name];
+                } catch(e){}
+            }
+            throw new Error(`Cannot find module '${name}.(${allowedRequireExtensions.join('/')})'`)
+        }
+    )
+})([
+    'js',
+    'json5',
+    'json',
+]);
 /**
  * @type {Rules}
  */
 module.exports = new Proxy({}, {
     get(_, name){
-        return require(`${__dirname}/${name}.js`);
+        return safeRequire(`${__dirname}/${name}`);
     },
     set(_, __, ___){}
 })
